b0c090ae1e2b66b2af536f0a68d3b28abd7aa49a
odoo-winspireweb <odoo.winspireweb@gmail.com>
2017-06-06 23:22:00 +0530

    [ADD] Directory v10 with blank module added

---
+++ v10/subscription_extended/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Recurring Documents Plus',
+    'version': '1.5',
+    'category': 'Customization',
+    'summary': '',
+    'description': """
+            Subscription Extended Module for Odoo V10
+    """,
+    'author' : 'ZedeS Technologies',
+    'website' : 'http://www.zedestech.com',
+    'depends': ['subscription', 'sale'],
+    'data': [],
+    'demo': [],
+    'test': [],
+    'update_xml': [
+
+        ],
+    'installable': True,
+    'auto_install': False,
+}
